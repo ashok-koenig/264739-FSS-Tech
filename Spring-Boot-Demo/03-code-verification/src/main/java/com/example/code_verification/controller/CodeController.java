99ef2285df33ffc6d668ca82ff88c5bd82c26855
Ashok S <ashok.s@koenig-solutions.com>
2026-07-20 17:00:19 +0530

    logging and actuator

--- Spring-Boot-Demo/03-code-verification/src/main/java/com/example/code_verification/controller/CodeController.java
+++ Spring-Boot-Demo/03-code-verification/src/main/java/com/example/code_verification/controller/CodeController.java
@@ -1,6 +1,8 @@
 package com.example.code_verification.controller;
 
 import com.example.code_verification.service.CodeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class CodeController {
     private CodeService codeService;
+    private static Logger logger = LoggerFactory.getLogger(CodeController.class);
 
     public CodeController(CodeService codeService) {
         this.codeService = codeService;
@@ -23,6 +26,7 @@ public class CodeController {
     @RequestMapping("/generate")
     public String generate(Model model){
         String code = codeService.generate();
+        logger.info("Generated code: "+ code);
         model.addAttribute("code", code);
         return "generate";
     }
@@ -36,6 +40,11 @@ public class CodeController {
     public String validateCode(@RequestParam(required = true) String code, Model model){
         boolean result = codeService.validate(code);
         model.addAttribute("result", result);
+        if(result){
+            logger.info("Validation Success");
+        }else{
+            logger.warn("Validation Failed with code: "+ code);
+        }
         return "validate-code";
     }
 }
