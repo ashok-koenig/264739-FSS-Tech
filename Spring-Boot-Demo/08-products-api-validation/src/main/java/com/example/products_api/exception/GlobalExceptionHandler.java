e78cd73520f65a173f9e2a6fac7a8b6d65280532
Ashok S <ashok.s@koenig-solutions.com>
2026-07-21 15:47:01 +0530

    validation demo

---
+++ Spring-Boot-Demo/08-products-api-validation/src/main/java/com/example/products_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,56 @@
+package com.example.products_api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<Object> handleProductNotFoundException(ProductNotFoundException e, WebRequest req){
+        Map<String, Object> errorBody = new HashMap<>();
+        errorBody.put("timestamp", LocalDateTime.now());
+        errorBody.put("status", HttpStatus.NOT_FOUND.value());
+        errorBody.put("error", "Product Not Found");
+        errorBody.put("message", e.getMessage());
+        errorBody.put("path", req.getDescription(false));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorBody);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGenericException(Exception e, WebRequest req){
+        Map<String, Object> errorBody = new HashMap<>();
+        errorBody.put("timestamp", LocalDateTime.now());
+        errorBody.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorBody.put("error", "Internal Server Error");
+        errorBody.put("message", e.getMessage());
+        errorBody.put("path", req.getDescription(false));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorBody);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException e, WebRequest req){
+        Map<String, Object> errorBody = new HashMap<>();
+        errorBody.put("timestamp", LocalDateTime.now());
+        errorBody.put("status", HttpStatus.BAD_REQUEST.value());
+        errorBody.put("error", "Input Validation Failed");
+        Map<String, Object> validationErrors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach( (error)-> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            validationErrors.put(fieldName, errorMessage);
+        });
+        errorBody.put("message", validationErrors);
+        errorBody.put("path", req.getDescription(false));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorBody);
+    }
+}
